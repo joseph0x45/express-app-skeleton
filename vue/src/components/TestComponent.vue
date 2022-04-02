c3a74ce97b0a2771b866a3cdef30c841e784c6e1
TheWisePigeon <zozozozeph@gmail.com>
2022-04-02 23:52:26 +0000

    Update TestComponent.vue

---
+++ vue/src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data(){
+        return {
+            number: 12
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <p>Bruh I love {{ number }} years olds</p>
+    </div>
+</template>
